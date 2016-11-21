08e4570a4b24f00130db2d2e36de7be0ab5c09b3
bmckalip <brmckalip@gmail.com>
2016-11-21 00:36:56 -0500

    done

--- program.py
+++ program.py
@@ -2,6 +2,11 @@ import tkinter as tk
 from tkinter import ttk
 
 
+class polyFrame(tk.Canvas):
+    def __init__(self, parent, *args, **kwargs):
+        tk.Canvas.__init__(self, master=parent, *args, **kwargs)
+
+
 class Graph(tk.Frame):
     DEFAULT_NUM_NODES = 10
 
@@ -18,25 +23,91 @@ class Graph(tk.Frame):
         vertexList = []
         return vertexList
 
-class Animation(tk.Tk):
-    algorithms = ['DFS', 'BFS']
+class Triangulation(tk.Tk):
+    algorithms = ['diagonal', 'monotone', 'ear']
 
-    def __init__(self, *args, **kwargs):
-        self.algorithm = None
-        try:
-            if kwargs['algo'] in Animation.algorithms:
-                self.algorithm = kwargs['algo']
-        except KeyError:
-            self.algorithms = 'DFS'
+    def algoIter(self):
+        if not self.diagonals:
+            self.triangulate()
+        else:
+            self.polygon.create_line(self.diagonals.pop(0))
+            self.polygon.pack()
 
+    def __init__(self, algo='diagonal', *args, **kwargs):
+        self.algorithm = algo
+        self.vertices = [[100, 0, 200, 100],
+                         [200, 100, 150, 150],
+                         [150, 150, 150, 250],
+                         [150, 250, 100, 150],
+                         [100, 150, 50, 250],
+                         [50, 250, 0, 100],
+                         [0, 100, 100, 0]
+                         ]
+
+        self.diagonals = []
         tk.Tk.__init__(self, *args, **kwargs)
-        container = tk.Frame(self, width=1600, height=900)
-        container.grid(row=0, column=0, sticky=tk.NSEW)
-        container.grid_rowconfigure(0, weight=1)
-        container.grid_columnconfigure(0, weight=1)
+        self.container = tk.Frame(self)
+        self.graph = tk.Frame(self.container)
+        self.header = tk.Frame(self.container)
+        self.footer = tk.Frame(self.container)
+        self.polygon = polyFrame(self.graph)
 
-        self.graph = Graph()
+        self.container.grid(row=0, column=0, sticky=tk.NSEW)
+        self.header.grid(row=0, column=0, sticky=tk.NSEW)
+        self.graph.grid(row=1, column=0, sticky=tk.NSEW)
+        self.footer.grid(row=2, column=0, sticky=tk.NSEW)
 
-ani = Animation()
-ani.title("Herb Prices")
-ani.mainloop()
+        self.container.grid_rowconfigure(0, weight=1)
+        self.container.grid_columnconfigure(0, weight=1)
+
+        self.buildGraph()
+        self.triangulate()
+
+        self.nameLabel = tk.Label(self.header, text=self.algorithm, font='bold')
+        self.nameLabel.pack()
+
+        btnEar = ttk.Button(self.footer, text='Iterate Through Algorithm', command=self.algoIter)
+        btnEar.pack(padx=5, pady=5)
+
+    def buildGraph(self):
+        try:
+            for vertex in self.vertices:
+                # polygon.create_polygon(vertex, outline='black', fill='White')
+                self.polygon.create_line(vertex)
+                self.polygon.pack()
+        except IndexError:
+            return 0
+
+    def triangulate(self):
+
+        if not self.algorithm:
+            return
+
+        elif self.algorithm == 'ear':
+            self.diagonals = [[0, 100, 200, 100],
+                              [0, 100, 100, 150],
+                              [100, 150, 150, 150],
+                              [100, 150, 200, 100]]
+        elif self.algorithm == 'diagonal':
+            self.diagonals = [[100, 0, 50, 250],
+                              [100, 0, 100, 150],
+                              [100, 0, 150, 250],
+                              [100, 0, 150, 150]]
+        elif self.algorithm == 'monotone':
+            self.diagonals = [[0, 100, 100, 150],
+                              [100, 150, 100, 0],
+                              [100, 0, 150, 250],
+                              [100, 0, 150, 150]]
+
+sim1 = Triangulation('ear')
+sim2 = Triangulation('diagonal')
+sim3= Triangulation('monotone')
+
+sim1.title('Ear Based Triangulation Algorithm')
+sim2.title('Diagonal Based Triangulation Algorithm')
+sim3.title('Monotone Based Triangulation Algorithm')
+
+sim1.mainloop()
+sim2.mainloop()
+sim3.mainloop()
+
